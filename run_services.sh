d285428bfa09be01601892a6a33443b5c0a230ad
Aswath <54670655+aswath01@users.noreply.github.com>
2023-12-17 23:03:53 +0530

    Update run_services.sh

--- run_services.sh
+++ run_services.sh
@@ -3,5 +3,5 @@ folders=("api_gateway" "user_service" "assignment_service")
 current_dir=$(pwd)
 # Loop through the folders and open terminals, running 'npm run dev'
 for folder in "${folders[@]}"; do
-  osascript -e "tell application \"Terminal\" to do script \"cd $current_dir/$folder && npm run dev\""
+  cd "$current_dir/$folder" && npm run dev
 done
