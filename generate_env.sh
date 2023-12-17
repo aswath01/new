572adee9296809f782491faea3642a207c2e3004
Aswath <54670655+aswath01@users.noreply.github.com>
2023-12-17 23:38:02 +0530

    Update generate_env.sh

--- generate_env.sh
+++ generate_env.sh
@@ -9,9 +9,9 @@ folders=(
 for folder in "${folders[@]}"; do
   env_file="$folder/.env"
   echo "Creating $env_file"
-  echo "USER_SERVICE_URL=http://localhost:3001" > "$env_file"
+  echo "USER_SERVICE_URL=http://user_service:3001" > "$env_file"
   echo "CLASSROOM_SERVICE_URL=http://localhost:3002" >> "$env_file"
-  echo "ASSIGNMENT_SERVICE_URL=http://localhost:3003" >> "$env_file"
+  echo "ASSIGNMENT_SERVICE_URL=http://assignment_service:3003" >> "$env_file"
   echo "AUTHENTICATE_FB_KEY=AIzaSyBtGuIOhOR6D1difgbeaPQtLtHYwwGe5wM" >> "$env_file"
   echo "JWT_SECRET_KEY=AIzaSyBtGuIOhOR6D1difgbeaPQtLtHYwwGe5wM" >> "$env_file"
 done
